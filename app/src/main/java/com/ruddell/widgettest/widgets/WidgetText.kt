906cea8ed40c2478c607429df22c8c3fb48f8228
cruddell <fH6CcNRT>
2022-09-21 08:05:34 -0500

    Sample project setup

---
+++ app/src/main/java/com/ruddell/widgettest/widgets/WidgetText.kt
@@ -0,0 +1,11 @@
+package com.ruddell.widgettest.widgets
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.glance.text.Text
+
+@Preview
+@Composable
+fun WidgetText() {
+    Text(text = "Hello world")
+}
